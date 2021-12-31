4971bc040996c3dd409cf2767ccd8b0a8bc961d5
Andrew Richerson <ajorigami1@gmail.com>
2021-12-30 21:38:01 -0600

    Make project fields nullable to support creating incomplete type

--- server/src/domain/project.ts
+++ server/src/domain/project.ts
@@ -1,8 +1,8 @@
 export type Project = {
-    id: string;
-    name: string;
-    description: string;
+    id?: string;
+    name?: string;
+    description?: string;
     /** An ordered array containing the ids of the columsn belonging to this project. */
-    columns: string[];
-    createdAt: string;
+    columns?: string[];
+    createdAt?: string;
 };
