2fad4a9b5aff1dc3f07cc269b8e9c1b0b408074b
Andrew Richerson <ajorigami1@gmail.com>
2022-01-03 14:58:56 -0600

    Flesh out column ui

--- frontend/src/ui/Column/ColumnCard.tsx
+++ frontend/src/ui/Column/ColumnCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Typography, Box } from "@mui/material";
+import { Card, Typography, Box, IconButton } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { SxProps } from "@mui/system";
 import { columnStyles } from "../shared/columnStyles";
@@ -7,6 +7,9 @@ import { Column as ColumnModel } from "../../domain/column";
 
 type StylesType = {
     container: SxProps;
+    header: SxProps & {
+        info: SxProps;
+    };
     content: SxProps;
 };
 
@@ -14,16 +17,23 @@ const styles: StylesType = {
     container: {
         ...columnStyles,
         display: "flex",
-        justifyContent: "center",
-        alignContent: "center",
+        flexDirection: "column",
+        padding: 1,
+    },
+    header: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        info: {
+            display: "flex",
+            "& > *": {
+                fontSize: "1.25rem",
+                marginRight: 1,
+            },
+        },
     },
     content: {
-        display: "flex",
-        alignSelf: "center",
-        "&:hover": {
-            textDecoration: "underline",
-            cursor: "pointer",
-        },
+        backgroundColor: "red",
     },
 };
 
@@ -32,9 +42,24 @@ type ColumnProps = {
 };
 
 export const ColumnCard = ({ column }: ColumnProps) => {
+    const handleAddIssue = () => {
+        console.log("add issue");
+    };
+
     return (
         <Card sx={styles.container} variant="outlined">
-            <Box sx={styles.content}>{column.name}</Box>
+            <Box sx={styles.header}>
+                <Box sx={styles.header.info}>
+                    <Typography>{column.issues?.length ?? 0}</Typography>
+                    <Typography>{column.name}</Typography>
+                </Box>
+                <Box>
+                    <IconButton onClick={handleAddIssue}>
+                        <AddIcon />
+                    </IconButton>
+                </Box>
+            </Box>
+            <Box sx={styles.content}>content</Box>
         </Card>
     );
 };
