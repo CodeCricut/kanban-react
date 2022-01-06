2892c218e3dd78319569482b668285e523812548
Andrew Richerson <ajorigami1@gmail.com>
2022-01-06 14:44:54 -0600

    Update column controller to use new handlers

--- server/src/controllers/columnController.ts
+++ server/src/controllers/columnController.ts
@@ -1,95 +1,73 @@
 import { NextFunction, Request, Response } from "express";
-import {
-    AddIssueToColumnCommand,
-    AddIssueToColumnHandler,
-} from "../application/commands/addIssueToColumn";
-import {
-    DeleteColumnCommand,
-    DeleteColumnHandler,
-} from "../application/commands/deleteColumn";
-import {
-    EditColumnCommand,
-    EditColumnHandler,
-} from "../application/commands/editColumn";
-import { GetColumnDto } from "../application/contracts/column";
-import { GetIssueDto } from "../application/contracts/issue";
-import {
-    GetColumnsIssuesHandler,
-    GetColumnsIssuesQuery,
-} from "../application/queries/getColumnsIssues";
+import { handleAddIssueToColumnCommand } from "../application/commands/addIssueToColumn";
+import { handleDeleteColumnCommand } from "../application/commands/deleteColumn";
+import { handleEditColumnCommand } from "../application/commands/editColumn";
+import { handleGetColumnsIssuesQuery } from "../application/queries/getColumnsIssues";
 
-export class ColumnController {
-    constructor(
-        private editColumnHandler: EditColumnHandler,
-        private deleteColumnHandler: DeleteColumnHandler,
-        private addIssueHandler: AddIssueToColumnHandler,
-        private getColumnIssuesHandler: GetColumnsIssuesHandler
-    ) {}
-
-    editColumn = async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const command: EditColumnCommand = {
-                id: req.params.id,
-                name: req.body.name,
-                description: req.body.description,
-            };
-            const updated: GetColumnDto = await this.editColumnHandler.handle(
-                command
-            );
+export async function editColumn(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    return handleEditColumnCommand({
+        id: req.params.id,
+        name: req.body.name,
+        description: req.body.description,
+    })
+        .then((updated) => {
             res.status(200);
-            return res.json(updated);
-        } catch (e: any) {
-            next(e);
-        }
-    };
+            res.json(updated);
+        })
+        .catch(next);
+}
 
-    deleteColumn = async (req: Request, res: Response, next: NextFunction) => {
-        const projectId: string = req.query.projectId as string;
-        try {
-            const command: DeleteColumnCommand = {
-                id: req.params.id,
-                projectId,
-            };
-            await this.deleteColumnHandler.handle(command);
-            return res.send();
-        } catch (e: any) {
-            next(e);
-        }
-    };
+export async function deleteColumn(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    return handleDeleteColumnCommand({
+        id: req.params.id,
+        projectId: req.query.projectId as string,
+    }).then((updated) => {
+        res.status(200);
+        return res.json(updated);
+    });
+}
 
-    addIssue = async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const { issueIndex, name, description, createdAt } = req.body;
-            const command: AddIssueToColumnCommand = {
-                columnId: req.params.id,
-                issueIndex,
-                name,
-                description,
-                createdAt,
-            };
-            const updatedColumn = await this.addIssueHandler.handle(command);
+export async function addIssue(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        const { issueIndex, name, description, createdAt } = req.body;
+        const updatedColumn = await handleAddIssueToColumnCommand({
+            columnId: req.params.id,
+            issueIndex,
+            name,
+            description,
+            createdAt,
+        });
+
+        res.status(200);
+        return res.json(updatedColumn);
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function getColumnIssues(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    return handleGetColumnsIssuesQuery({
+        columnId: req.params.id,
+    })
+        .then((issues) => {
             res.status(200);
-            return res.json(updatedColumn);
-        } catch (e: any) {
-            next(e);
-        }
-    };
-
-    getColumnIssues = async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ) => {
-        try {
-            const query: GetColumnsIssuesQuery = {
-                columnId: req.params.id,
-            };
-            const colIssues: GetIssueDto[] =
-                await this.getColumnIssuesHandler.handle(query);
-            res.status(200);
-            return res.json(colIssues);
-        } catch (e: any) {
-            next(e);
-        }
-    };
+            return res.json(issues);
+        })
+        .catch(next);
 }
