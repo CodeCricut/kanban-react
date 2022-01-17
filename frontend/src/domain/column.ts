4e88cfd8c9380aafb28e109609096b5ba5a4a6a3
Andrew Richerson <ajorigami1@gmail.com>
2022-01-17 11:17:02 -0600

    Update frontend entities to be embedded documents

--- frontend/src/domain/column.ts
+++ frontend/src/domain/column.ts
@@ -1,8 +1,10 @@
+import { Issue } from "./issue";
+
 export type Column = {
     id?: string;
     name?: string;
     description?: string;
-    issues?: string[];
+    issues?: Issue[];
     createdAt?: string;
 };
 
