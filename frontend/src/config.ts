e783dcda6901dd1a3023fbed681f26986e822dd2
Andrew Richerson <ajorigami1@gmail.com>
2022-01-18 20:02:01 -0600

    Simplify and update api route config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -8,88 +8,28 @@ if (process.env.NODE_ENV === "development") {
 
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
-// TODO: Remove the AppConfig type and just have a global config object
+export const appConfig = {
+    registerRoute: "/auth/register",
+    loginRoute: "/auth/login",
 
-export type AppConfig = {
-    projectsRoute: string;
-    createProjectRoute: string;
-    getAllProjectsRoute: string;
-    editProjectRoute: (id: string) => string;
-    deleteProjectRoute: (id: string) => string;
-    addColumnToProjectRoute: (projectId: string) => string;
-    getProjectColumnsRoute: (projectId: string) => string;
-    editColumnRoute: (columnId: string) => string;
-    deleteColumnRoute: (id: string, projectId: string) => string;
-    reorderColumnRoute: (
-        projectId: string,
-        columnid: string,
-        newIndex: number
-    ) => string;
-    addIssueRoute: (columnId: string) => string;
-    getColumnIssuesRoute: (columnId: string) => string;
-    editIssueRoute: (issueId: string) => string;
-    deleteIssueRoute: (id: string, columnId: string) => string;
-    relocateIssueRoute: (id: string) => string;
+    getMeRoute: "/users/me",
+    getMyProjectsRoute: "/users/me/projects",
 
-    registerRoute: string;
-    loginRoute: string;
-
-    getMeRoute: string;
-
-    getMyProjectsRoute: string;
-};
-
-const projectsRoute = "/projects";
-const createProjectRoute = `${projectsRoute}/create`;
-const getAllProjectsRoute = "/projects";
-const editProjectRoute = (id: string) => `/projects/edit/${id}`;
-const deleteProjectRoute = (id: string) => `/projects/delete/${id}`;
-const addColumnToProjectRoute = (projectId: string) =>
-    `/projects/add-column/${projectId}`;
-const getProjectColumnsRoute = (projectId: string) =>
-    `/projects/columns/${projectId}`;
-
-const editColumnRoute = (columnId: string) => `/columns/edit/${columnId}`;
-const deleteColumnRoute = (id: string, projectId: string) =>
-    `/columns/delete/${id}?projectId=${projectId}`;
-const reorderColumnRoute = (
-    projectId: string,
-    columnId: string,
-    newIndex: number
-) =>
-    `/projects/reorder-column/${projectId}?columnId=${columnId}&newIndex=${newIndex}`;
-const addIssueRoute = (columnId: string) => `/columns/add-issue/${columnId}`;
-const getColumnIssuesRoute = (columnId: string) =>
-    `/columns/issues/${columnId}`;
-const editIssueRoute = (issueId: string) => `/issues/edit/${issueId}`;
-const deleteIssueRoute = (id: string, columnId: string) =>
-    `/issues/delete/${id}?columnId=${columnId}`;
-const relocateIssueRoute = (id: string) => `/issues/relocate/${id}`;
-
-const registerRoute = "/auth/register"
-const loginRoute = "/auth/login";
-const getMeRoute = "/users/me"
-
-const getMyProjectsRoute = "/users/me/projects"
-
-export const appConfig: AppConfig = {
-    projectsRoute,
-    createProjectRoute,
-    getAllProjectsRoute,
-    editProjectRoute,
-    deleteProjectRoute,
-    addColumnToProjectRoute,
-    getProjectColumnsRoute,
-    editColumnRoute,
-    deleteColumnRoute,
-    reorderColumnRoute,
-    addIssueRoute,
-    getColumnIssuesRoute,
-    editIssueRoute,
-    deleteIssueRoute,
-    relocateIssueRoute,
-    registerRoute,
-    loginRoute,
-    getMeRoute,
-    getMyProjectsRoute
+    projectsRout: "/projects",
+    createProjectRoute: "/projects/create",
+    editProjectRoute: (projectId: string) => `/projects/edit/${projectId}`,
+    deleteProjectRoute: (projectId: string) => `/projects/delete/${projectId}`,
+    addColumnRoute: (projectId: string) => `/projects/add-column/${projectId}`,
+    relocateColumnRoute: (projectId: string) =>
+        `/projects/relocate-column/${projectId}`,
+    editColumnRoute: (projectId: string) =>
+        `/projects/edit-column/${projectId}`,
+    deleteColumnRoute: (projectId: string, columnId: string) =>
+        `/projects/delete/${projectId}?columnId=${columnId}`,
+    addIssueRoute: (projectId: string) => `/projects/add-issue/${projectId}`,
+    editIssueRoute: (projectId: string) => `/projects/edit-issue/${projectId}`,
+    deleteIssueRoute: (projectId: string, issueId: string) =>
+        `/projects/delete-issue/${projectId}?issueId=${issueId}`,
+    relocateIssueRoute: (projectId: string) =>
+        `/projects/relocate-issue/${projectId}`,
 };
