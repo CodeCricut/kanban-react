4d3cb7cc7d9490ae2f403b7bd01275573591a2df
Andrew Richerson <ajorigami1@gmail.com>
2022-01-18 20:47:27 -0600

    Clean up get users projects feature

--- frontend/src/application/getUsersProjects/hook.tsx
+++ frontend/src/application/getUsersProjects/hook.tsx
@@ -5,29 +5,32 @@ import { useProjectsStorage } from "../../services/projectsStorage";
 import { useUserApiService } from "../../services/userApi/hook";
 import { getUsersProjects } from "./getIsersProjects";
 
-
 type GetUsersProjectsFunction = {
-    (): Promise<Project[]|undefined>
-}
+    (): Promise<Project[] | undefined>;
+};
 export function useGetUsersProjects(): GetUsersProjectsFunction {
-    const jwtStorageService = useJwtStorageService();
     const usersApiService = useUserApiService();
 
-    const func = useCallback(() => getUsersProjects({
-        jwtStorageService,
-        usersApiService
-    }), [jwtStorageService, usersApiService])
+    const func = useCallback(
+        () =>
+            getUsersProjects({
+                usersApiService,
+            }),
+        [usersApiService]
+    );
 
     return func;
 }
 
 export function useUsersProjects(): Project[] {
     const getUsersProjects = useGetUsersProjects();
-    const projectStorageService = useProjectsStorage()
-    
+    const projectStorageService = useProjectsStorage();
+
     // Reset the local project state whenever getUsersProjects changes, such as when the jwt is changed
     useEffect(() => {
-        getUsersProjects().then(loaded => projectStorageService.updateProjects(loaded ?? []))
+        getUsersProjects().then((loaded) =>
+            projectStorageService.updateProjects(loaded ?? [])
+        );
     }, [getUsersProjects]);
 
     return projectStorageService.projects;
