ee32498ac4f8b0d74cbe8d9a8a6dcd6300dc26e0
Andrew Richerson <ajorigami1@gmail.com>
2022-01-18 20:47:39 -0600

    Clean up

--- frontend/src/application/getUsersProjects/getIsersProjects.ts
+++ frontend/src/application/getUsersProjects/getIsersProjects.ts
@@ -3,11 +3,8 @@ import { IJwtStorageService } from "../contracts/jwtStorageService";
 import { IUserApiService } from "../contracts/userApiService";
 
 type Dependencies = {
-    jwtStorageService: IJwtStorageService;
     usersApiService: IUserApiService
 }
-export async function getUsersProjects({jwtStorageService, usersApiService}: Dependencies): Promise<Project[]|undefined> {
-    const jwt = jwtStorageService.jwt;
-    if (!jwt) return undefined;
-    return await usersApiService.getMyProjects(jwt)
+export async function getUsersProjects({usersApiService}: Dependencies): Promise<Project[]|undefined> {
+    return await usersApiService.getMyProjects()
 }
