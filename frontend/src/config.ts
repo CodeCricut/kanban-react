9fd8f3792c921c2f7ff87cf5a1eebf1d3ebfe396
Andrew Richerson <ajorigami1@gmail.com>
2022-03-01 11:17:09 -0600

    Configure production api url for frontend as part of #80

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 if (process.env.NODE_ENV === "development") {
     axios.defaults.baseURL = `http://localhost:3001/api`;
 } else if (process.env.NODE_ENV === "production") {
-    axios.defaults.baseURL = `api`;
+    axios.defaults.baseURL = `https://codecricut-kanban.herokuapp.com/api`;
 }
 
 axios.defaults.headers.post["Content-Type"] = "application/json";
