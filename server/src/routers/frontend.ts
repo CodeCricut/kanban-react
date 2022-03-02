fb5ea8bf37e91e42c98920dc7daf0f40ae0c547b
Andrew Richerson <ajorigami1@gmail.com>
2022-03-01 21:15:23 -0600

    Fix route api issue on Heroku

--- server/src/routers/frontend.ts
+++ server/src/routers/frontend.ts
@@ -6,7 +6,7 @@ const frontendRouter = Router();
 
 // This could go in a controller, but it is so simple that I don't think it warrants it
 frontendRouter.use(express.static(config.frontendBuildPath));
-frontendRouter.get("/", (req: Request, res: Response) => {
+frontendRouter.get("/*", (req: Request, res: Response) => {
     res.sendFile(config.frontendIndexPath);
 });
 
